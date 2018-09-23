9f6bd52d6d1908cd2dce62b958f398b20eaf16a9
Lam Ngoc Khuong <lamngockhuong@gmail.com>
2018-09-23 23:02:42 +0700

    Init project

---
+++ src/main/java/com/ngockhuong/app/SpringbootActivemqExampleApplication.java
@@ -0,0 +1,14 @@
+package com.ngockhuong.app;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+public class SpringbootActivemqExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActivemqExampleApplication.class, args);
+    }
+}
